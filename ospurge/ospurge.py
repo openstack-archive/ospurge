b98133317cb4662c03e6bb65db49c27a7bb1bded
Yves-Gwenael Bourhis <yves-gwenael.bourhis@cloudwatt.com>
2015-03-27 17:43:09 +0100

    Ignore improperly configured urls
    We do not fail if an endpoint url is not properly configured.
    
    Closes Bug: #1438062
    
    Change-Id: Ia0af30d4f882daa200703416bcb8530a5a4c6343

--- ospurge/ospurge.py
+++ ospurge/ospurge.py
@@ -750,6 +750,10 @@ def perform_on_project(admin_name, password, project, auth_url,
                 ResourceNotEnabled):
             # If service is not in Keystone's services catalog, ignoring it
             pass
+        except requests.exceptions.MissingSchema as e:
+            logging.warning(
+                'Some resources may not have been deleted, "%s" is improperly '
+                'configured and returned: %s\n' % (rc, e))
         except (ceilometerclient.exc.InvalidEndpoint, glanceclient.exc.InvalidEndpoint) as e:
             logging.warning(
                 "Unable to connect to {} endpoint : {}".format(rc, e.message))
