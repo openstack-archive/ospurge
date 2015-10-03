a6d6a95f030561ce677a63882fd5da272096e164
Christian Berendt <berendt@b1-systems.de>
2015-10-03 21:30:56 +0200

    Move decorator and base classes into a separate module
    Change-Id: Iac555cdf656199c6f0f3d2d132c286e9e4edce23

--- ospurge/client.py
+++ ospurge/client.py
@@ -28,7 +28,6 @@ from distutils import version
 import logging
 import os
 import sys
-import time
 
 import ceilometerclient.exc
 from ceilometerclient.v2 import client as ceilometer_client
@@ -48,128 +47,12 @@ import novaclient.exceptions
 import requests
 from swiftclient import client as swift_client
 
+from ospurge import base
 from ospurge import constants
 from ospurge import exceptions
 
 
-# Decorators
-
-def retry(service_name):
-    def factory(func):
-        """Decorator allowing to retry in case of failure."""
-        def wrapper(*args, **kwargs):
-            n = 0
-            while True:
-                try:
-                    return func(*args, **kwargs)
-                except Exception as e:
-                    if getattr(e, 'http_status', False) == 404:
-                        # Sometimes a resource can be deleted manually by
-                        # someone else while ospurge is running and listed it.
-                        # If this happens, We raise a Warning.
-                        logging.warning(
-                            "Can not delete the resource because it does not"
-                            " exist : %s" % e
-                        )
-                        # No need to retry deleting an non existing resource
-                        break
-                    else:
-                        if n == constants.RETRIES:
-                            raise exceptions.DeletionFailed(service_name)
-                        n += 1
-                        logging.info("* Deletion failed - "
-                                     "Retrying in {} seconds - "
-                                     "Retry count {}".format(constants.TIMEOUT, n))
-                        time.sleep(constants.TIMEOUT)
-        return wrapper
-    return factory
-
-
-# Classes
-class Session(object):
-
-    """A Session stores information that can be used by the different Openstack Clients.
-
-    The most important data is:
-    * self.token - The Openstack token to be used accross services;
-    * self.catalog - Allowing to retrieve services' endpoints.
-    """
-
-    def __init__(self, username, password, project_id, auth_url,
-                 endpoint_type="publicURL", region_name=None, insecure=False):
-        client = keystone_client.Client(
-            username=username, password=password, tenant_id=project_id,
-            auth_url=auth_url, region_name=region_name, insecure=insecure)
-        # Storing username, password, project_id and auth_url for
-        # use by clients libraries that cannot use an existing token.
-        self.username = username
-        self.password = password
-        self.project_id = project_id
-        self.auth_url = auth_url
-        self.region_name = region_name
-        self.insecure = insecure
-        # Session variables to be used by clients when possible
-        self.token = client.auth_token
-        self.user_id = client.user_id
-        self.project_name = client.project_name
-        self.endpoint_type = endpoint_type
-        self.catalog = client.service_catalog.get_endpoints()
-        try:
-            # Detect if we are admin or not
-            client.roles.list()  # Only admins are allowed to do this
-        except (
-            # The Exception Depends on OpenStack Infrastructure.
-            api_exceptions.Forbidden,
-            api_exceptions.ConnectionRefused,  # admin URL not permitted
-            api_exceptions.Unauthorized,
-        ):
-            self.is_admin = False
-        else:
-            self.is_admin = True
-
-    def get_endpoint(self, service_type):
-        try:
-            return self.catalog[service_type][0][self.endpoint_type]
-        except (KeyError, IndexError):
-            # Endpoint could not be found
-            raise exceptions.EndpointNotFound(service_type)
-
-
-class Resources(object):
-
-    """Abstract base class for all resources to be removed."""
-
-    def __init__(self, session):
-        self.session = session
-
-    def list(self):
-        pass
-
-    def delete(self, resource):
-        """Displays informational message about a resource deletion."""
-        logging.info("* Deleting {}.".format(self.resource_str(resource)))
-
-    def purge(self):
-        """Delete all resources."""
-        # Purging is displayed and done only if self.list succeeds
-        resources = self.list()
-        c_name = self.__class__.__name__
-        logging.info("* Purging {}".format(c_name))
-        for resource in resources:
-            retry(c_name)(self.delete)(resource)
-
-    def dump(self):
-        """Display all available resources."""
-        # Resources type and resources are displayed only if self.list succeeds
-        resources = self.list()
-        c_name = self.__class__.__name__
-        print("* Resources type: {}".format(c_name))
-        for resource in resources:
-            print(self.resource_str(resource))
-        print("")
-
-
-class SwiftResources(Resources):
+class SwiftResources(base.Resources):
 
     def __init__(self, session):
         super(SwiftResources, self).__init__(session)
@@ -217,7 +100,7 @@ class SwiftContainers(SwiftResources):
         return "container {}".format(obj)
 
 
-class CinderResources(Resources):
+class CinderResources(base.Resources):
 
     def __init__(self, session):
         super(CinderResources, self).__init__(session)
@@ -277,7 +160,7 @@ class CinderBackups(CinderResources):
         return "backup {} (id {}) of volume {}".format(backup.name, backup.id, backup.volume_id)
 
 
-class NeutronResources(Resources):
+class NeutronResources(base.Resources):
 
     def __init__(self, session):
         super(NeutronResources, self).__init__(session)
@@ -557,7 +440,7 @@ class NeutronFireWall(NeutronResources):
         return "Firewall {} (id {})".format(firewall['name'], firewall['id'])
 
 
-class NovaServers(Resources):
+class NovaServers(base.Resources):
 
     def __init__(self, session):
         super(NovaServers, self).__init__(session)
@@ -581,7 +464,7 @@ class NovaServers(Resources):
         return "server {} (id {})".format(server.name, server.id)
 
 
-class GlanceImages(Resources):
+class GlanceImages(base.Resources):
 
     def __init__(self, session):
         self.client = glance_client.Client(
@@ -605,7 +488,7 @@ class GlanceImages(Resources):
         return res.owner == self.project_id
 
 
-class HeatStacks(Resources):
+class HeatStacks(base.Resources):
 
     def __init__(self, session):
         self.client = heat_client.Client(
@@ -628,7 +511,7 @@ class HeatStacks(Resources):
         return "stack {})".format(stack.id)
 
 
-class CeilometerAlarms(Resources):
+class CeilometerAlarms(base.Resources):
 
     def __init__(self, session):
         # Ceilometer Client needs a method that returns the token
@@ -739,8 +622,8 @@ def perform_on_project(admin_name, password, project, auth_url,
 
     action can be: 'purge' or 'dump'
     """
-    session = Session(admin_name, password, project, auth_url,
-                      endpoint_type, region_name, insecure)
+    session = base.Session(admin_name, password, project, auth_url,
+                           endpoint_type, region_name, insecure)
     error = None
     for rc in constants.RESOURCES_CLASSES:
         try:
