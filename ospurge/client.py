f04504653c504da411a1550b2ba465f91e9afd9b
Christian Berendt <berendt@b1-systems.de>
2015-11-09 15:21:09 +0100

    Move cinder resources into a separate module
    Change-Id: I1e0e711b8d68bef67dd0a98fd16bd1c2790c9def

--- ospurge/client.py
+++ ospurge/client.py
@@ -24,14 +24,11 @@
 # SOFTWARE.
 
 import argparse
-from distutils import version
 import logging
 import sys
 
 import ceilometerclient.exc
 from ceilometerclient.v2 import client as ceilometer_client
-import cinderclient
-from cinderclient.v1 import client as cinder_client
 import glanceclient.exc
 from glanceclient.v1 import client as glance_client
 from heatclient import client as heat_client
@@ -46,68 +43,9 @@ from ospurge import base
 from ospurge import constants
 from ospurge import exceptions
 # TODO(berendt): wildcard imports will be removed in the future
+from ospurge.resources.cinder import *  # noqa
 from ospurge.resources.swift import *  # noqa
 from ospurge import utils
-
-
-class CinderResources(base.Resources):
-
-    def __init__(self, session):
-        super(CinderResources, self).__init__(session)
-        # Cinder client library can't use an existing token. When
-        # using this library, we have to reauthenticate.
-        self.client = cinder_client.Client(
-            session.username, session.password,
-            session.project_name, session.auth_url, session.insecure,
-            endpoint_type=session.endpoint_type,
-            region_name=session.region_name)
-
-
-class CinderSnapshots(CinderResources):
-
-    def list(self):
-        return self.client.volume_snapshots.list()
-
-    def delete(self, snap):
-        super(CinderSnapshots, self).delete(snap)
-        self.client.volume_snapshots.delete(snap)
-
-    def resource_str(self, snap):
-        return "snapshot {} (id {})".format(snap.display_name, snap.id)
-
-
-class CinderVolumes(CinderResources):
-
-    def list(self):
-        return self.client.volumes.list()
-
-    def delete(self, vol):
-        """Snapshots created from the volume must be deleted first."""
-        super(CinderVolumes, self).delete(vol)
-        self.client.volumes.delete(vol)
-
-    def resource_str(self, vol):
-        return "volume {} (id {})".format(vol.display_name, vol.id)
-
-
-class CinderBackups(CinderResources):
-
-    def list(self):
-        if self.session.is_admin and version.LooseVersion(
-                cinderclient.version_info.version_string()) < '1.4.0':
-            logging.warning('cinder volume-backups are ignored when ospurge is '
-                            'launched with admin credentials because of the '
-                            'following bug: '
-                            'https://bugs.launchpad.net/python-cinderclient/+bug/1422046')
-            return []
-        return self.client.backups.list()
-
-    def delete(self, backup):
-        super(CinderBackups, self).delete(backup)
-        self.client.backups.delete(backup)
-
-    def resource_str(self, backup):
-        return "backup {} (id {}) of volume {}".format(backup.name, backup.id, backup.volume_id)
 
 
 class NeutronResources(base.Resources):
@@ -505,7 +443,6 @@ def perform_on_project(admin_name, password, project, auth_url,
             func()
         except (exceptions.EndpointNotFound,
                 neutronclient.common.exceptions.EndpointNotFound,
-                cinderclient.exceptions.EndpointNotFound,
                 novaclient.exceptions.EndpointNotFound,
                 heatclient.openstack.common.apiclient.exceptions.EndpointNotFound,
                 exceptions.ResourceNotEnabled):
