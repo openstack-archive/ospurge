7da6b9652bafbba15b8b8cf8300a7470222af574
Christian Berendt <berendt@b1-systems.de>
2015-11-09 21:48:07 +0100

    Fix syntax issues in bash scripts and add bashate check
    * [E] E003: Indent not multiple of 4
    
    Change-Id: I7cd5c8b5348de065d84d03a316dc120f121841f3

--- tools/utils.bash
+++ tools/utils.bash
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 function exit_on_failure {
-  RET_CODE=$?
-  ERR_MSG=$1
-  if [ $RET_CODE -ne 0 ]; then
-      echo $ERR_MSG
-      exit 1
-  fi
+    RET_CODE=$?
+    ERR_MSG=$1
+    if [ $RET_CODE -ne 0 ]; then
+        echo $ERR_MSG
+        exit 1
+    fi
 }
 
 function exit_if_empty {
-  STRING=${1:-}
-  ERR_MSG=${2:-}
-  if [ -z "$STRING" ]; then
-      echo $ERR_MSG
-      exit 1
-  fi
+    STRING=${1:-}
+    ERR_MSG=${2:-}
+    if [ -z "$STRING" ]; then
+        echo $ERR_MSG
+        exit 1
+    fi
 }
