80c716519f7bad0fe4f4ce13475da81c12fc8ecd
Tobias Urdin <tobias.urdin@binero.se>
2018-09-09 18:17:12 +0200

    Add heat functional testing
    Change-Id: I5c668d0b6dd960f9c74b57831e5cdb31167fdb51

---
+++ ospurge/resources/octavia.py
@@ -0,0 +1,29 @@
+#  Licensed under the Apache License, Version 2.0 (the "License"); you may
+#  not use this file except in compliance with the License. You may obtain
+#  a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#  License for the specific language governing permissions and limitations
+#  under the License.
+from ospurge.resources import base
+
+
+class Loadbalancer(base.ServiceResource):
+    ORDER = 81
+
+    def list(self):
+        if not self.cloud.has_service('load-balancer'):
+            return []
+        return self.cloud.list_stacks()
+
+    def delete(self, resource):
+        self.cloud.delete_stack(resource['id'], wait=True)
+
+    @staticmethod
+    def to_str(resource):
+        return "Heat Stack (id='{}', name='{}'".format(
+            resource['id'], resource['name'])
